655db1394ffb89f5c4698bb12d4671aa73e635f9
thanhgaming5550 <62001770+thanhgaming5550@users.noreply.github.com>
2022-08-12 09:10:35 +0700

    Update v1 to v2

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
 <h1 align="center">classic-chatbox-server</h1>
 
-<p align="center">Một chatbox đơn giản chỉ sử dụng API. Không dùng webhook hay websocket</p>
-<p align="center">Đây là Server của <b>classic-chatbox</b>. Click vào <a href="https://github.com/thanhgaming5550/classic-chatbox-client">đây</a> để chuyển sang Client</p>
+<p align="center">Một chatbox đơn giản sử dụng WebSocket. Là một phiên bản nâng cấp của <a href="https://github.com/thanhgaming5550/classic-chatbox-server/tree/api"> Bản API </a> để cập nhật theo thời gian thực.</p>
+<p align="center">Đây là Server của <b>classic-chatbox</b>. Tạm thời chưa có Client thích hợp.
+
+<!-- Click vào <a href="https://github.com/thanhgaming5550/classic-chatbox-client">đây</a> để chuyển sang Client</p> -->
 
 #### Yêu cầu:
 - <a href="https://pypi.org/project/Flask/">Flask</a>
+- <a href="https://pypi.org/project/flask-sock/">Flask-Sock</a>
 - <a href="https://www.python.org/">Python 3</a>
 
 #### Hướng dẫn sử dụng:
   Chạy file <a href="https://github.com/thanhgaming5550/classic-chatbox-server/blob/main/index.py">`index.py`</a> (`python index.py`)
 
+#### Đầu vào và đầu ra:
+Kết nối và gửi tin nhắn:
+  - Gửi: `{"type":"send", "data":{"name":"Tên_Người_Gửi", "content":"Nội_dung"}}`
+    - Thành công: 
+      - `{"type":"send", "status": true, "timestamp":"Xâu_thời_gian"}`cho client đã gửi tin
+      - `{"name":"Tên_Người_Gửi", "content":"Nội_dung", "timestamp":"Xâu_thời_gian}` cho các client khác
+
+      <b>CHÚ Ý: `"Tên_Người_Gửi"` từ 1 đến 100 ký tự, `"Nội_dung"` từ 1 đến 4000 ký tự</b>
+    - Thất bại:
+      - `{"type":"send", "status": false}` cho client đã gửi tin
+  - Lấy: `{"type":"get"}`
+    - Thành công: 
+      - `{"type":"get", "status": true, "data":[Tất cả tin nhắn]}`cho client đã gửi tin
+        - Đại diện 1 object trong `"data"`: 
+          `{"name":"Tên_Người_Gửi", "content":"Nội_dung", "timestamp":"Xâu_thời_gian"}`
+    - Thất bại: 
+      `{"type":"get", "status": false}`cho client đã gửi tin
+
+Nếu gửi tin không đúng định dạng, tự động ngắt kết nối WebSocket.
+
 #### Lưu trữ:
   Lịch sử chat sẽ được lưu trong file <a href="https://github.com/thanhgaming5550/classic-chatbox-server/blob/main/data.json">`data.json`</a> để lưu trữ và thực hiện các hành động liên quan. Rất cổ điển.
-  
-#### API:
-  Sau khi chạy file <a href="https://github.com/thanhgaming5550/classic-chatbox-server/blob/main/index.py">`index.py`</a>, truy cập vào địa chỉ mà file đấy nó chạy (VD: File `index.py` chạy và host ở  `http://127.0.0.1:5000` thì truy cập vào `http://127.0.0.1:5000`. <br><br>
-  Mọi hoạt động của API được chạy trên địa chỉ này
-
-#### Bên ngoài:
-Các file sau được sử dụng trong repo này mà chưa có sự cho phép của chủ sở hữu. Mình không phải là chủ sở hữu của các file này. Nếu chủ sở hữu muốn xóa nó khỏi repo, vui lòng liên hệ mình qua Messenger hoặc Discord.
-- <a href="https://github.com/thanhgaming5550/classic-chatbox-server/blob/main/static/assets/chino.jpg">`./static/assets/chino.jpg`</a> (Là background của <a href="https://osu.ppy.sh/beatmapsets/1677520#osu/3428447">cái này</a>)
-
 #### Thông tin khác:
 - **#study**: Đây là repo được tạo ra nhằm mục đích để hoàn thành Bài tập về nhà hoặc tựa tựa thế.
--- index.py
+++ index.py
@@ -1,85 +1,97 @@
-from flask import Flask, request, make_response, render_template
 import json
+from flask import Flask
+from flask_sock import Sock
 import datetime
 
 app = Flask(__name__)
+sock = Sock(app)
 
-@app.route("/send/", methods=['GET'])
-def send():
-    name = request.args['name']
-    content = request.args['content']
-    response = make_response()
-    if ((len(name)>0) & (len(name)<=100) & (len(content)>0) & (len(content)<=4000)):
-        try:
-            #*Read old data 
-            file = open("data.json", encoding='utf_8')
-            if (file.read().rstrip()==''):
-                up = open("data.json", mode='w', encoding='utf_8')
-                up.write('[]')
-                up.close()
-            file.close()
-            file = open("data.json", encoding='utf_8')
-            msg = json.loads(file.read().rstrip())
-            file.close()
+socks = []
 
-            #*Package data to Python list
-            timestamp = datetime.datetime.now()
-            input = {
-                "name": name,
-                "content": content,
-                "timestamp": timestamp.strftime("%d/%m/%Y, %H:%M:%S")
-            }   
-            msg.append(input)
+def get():
+    file = open("data.json", encoding='utf_8')
+    if (file.read().rstrip()==''):
+        up = open("data.json", mode='w', encoding='utf_8')
+        up.write('[]')
+        up.close()
+    file.close()
+    file = open("data.json", encoding='utf_8')
+    output = file.read().rstrip()
+    file.close()
+    return output
 
-            #*Save chat
-            file = open('data.json', mode = 'w', encoding='utf_8')
-            file.write(json.dumps(msg))
-            file.close()
-            print("["+timestamp.strftime("%d/%m/%Y, %H:%M:%S")+"] "+name+": "+content)
-
-            #*Return client
-            ject = {
-                "sent" : True,
-                "timestamp": timestamp.strftime("%d/%m/%Y, %H:%M:%S")
-            }
-        except:
-            ject = {
-                'sent':False
-            }
-    else: 
-        ject = {
-            'sent':False
-        }
-    response.set_data(json.dumps(ject))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-@app.route("/get/", methods=['GET'])
-def getslash():
-    response = make_response()
-    ject = {}
+@sock.route('/connect')
+def connect(ws):
+    socks.append(ws)
     try:
-        file = open("data.json", encoding='utf_8')
-        if (file.read().rstrip()==''):
-            up = open("data.json", mode='w', encoding='utf_8')
-            up.write('[]')
-            up.close()
-        file.close()
-        file = open("data.json", encoding='utf_8')
-        data = json.loads(file.read().rstrip())
-        file.close()
-        ject = {
-            "got" : True,
-            "data" : data
-        }
-    except:
-        ject = {"got" : False}
-    response.set_data(json.dumps(ject))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+        while True:
+            receive = ws.receive()
+            data = json.loads(receive)
+            
+            if (data['type'] == "send"):
+                name = data['data']['name']
+                content = data['data']['content']
+                if ((len(name)>0) & (len(name)<=100) & (len(content)>0) & (len(content)<=4000)):
+                    try:
+                        #*Read old data 
+                        msg = json.loads(get())
 
-@app.route('/')
-def index():
-    return render_template('main.html') #Thanks https://stackoverflow.com/questions/23327293/flask-raises-templatenotfound-error-even-though-template-file-exists
+                        #*Package data to Python list
+                        timestamp = datetime.datetime.now()
+                        input = {
+                            "name": name,
+                            "content": content,
+                            "timestamp": timestamp.strftime("%d/%m/%Y, %H:%M:%S")
+                        }   
+                        msg.append(input)
+
+                        #*Save chat
+                        file = open('data.json', mode = 'w', encoding='utf_8')
+                        file.write(json.dumps(msg))
+                        file.close()
+                        print("["+timestamp.strftime("%d/%m/%Y, %H:%M:%S")+"] "+name+": "+content)
+
+                        #*Return client send
+                        output = {
+                            "type": "send",
+                            "status": True,
+                            "timestamp": timestamp.strftime("%d/%m/%Y, %H:%M:%S")
+                        }
+                        ws.send(json.dumps(output))
+
+                        #*Update another Client
+                        for client in socks:
+                            if (client != ws):
+                                client.send(input)
+                    except:
+                        output = {
+                            "type": "send",
+                            "status": False,
+                        }
+                        ws.send(json.dumps(output))
+                else:
+                    output = {
+                            "type": "send",
+                            "status": False,
+                        }
+                    ws.send(json.dumps(output))
+                    
+            if (data['type'] == "get"):
+                output = {}
+                try:
+                    dat = json.loads(get())
+                    output = {
+                        "type": "get",
+                        "status": True,
+                        "data": dat
+                    }
+                except:
+                    output = {
+                        "type": "get",
+                        "status": False
+                    }
+                ws.send(json.dumps(output))
+    except:
+        socks.remove(ws)
 
 app.run()
